a6228d7feda0fb506dff8d050fd476ab9784a419
dominic_lian <dominic_lian@qq.com>
2018-07-22 21:59:17 +0800

    update  344

---
+++ Swift/344. Reverse String/Leet/Solution.swift
@@ -0,0 +1,13 @@
+//
+//  Solution.swift
+//  Leet
+//
+//  Created by dominic_lian on 2018/4/1.
+//  Copyright © 2018年 dominic_lian. All rights reserved.
+//
+
+class Solution {
+    func reverseString(_ s: String) -> String {
+        return String.init(s.reversed())
+    }
+}
---
+++ Swift/344. Reverse String/Leet/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Leet
+//
+//  Created by dominic_lian on 2018/4/9.
+//  Copyright © 2018年 dominic_lian. All rights reserved.
+//
+
+
+var str = "hello"
+
+let solution = Solution.init()
+
+print("\(solution.reverseString(str))")
+

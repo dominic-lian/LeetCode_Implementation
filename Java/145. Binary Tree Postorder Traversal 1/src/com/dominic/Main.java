cd5243459fd5153d1589481b9d7dad21efe5f896
dominic_lian <dominic_lian@qq.com>
2018-08-24 14:54:32 +0800

    update  145

---
+++ Java/145. Binary Tree Postorder Traversal 1/src/com/dominic/Main.java
@@ -0,0 +1,24 @@
+package com.dominic;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        TreeNode treeNode_1 = new TreeNode(3);
+
+        TreeNode treeNode_2 = new TreeNode(9);
+        TreeNode treeNode_3 = new TreeNode(20);
+
+        TreeNode treeNode_4 = new TreeNode(15);
+        TreeNode treeNode_5 = new TreeNode(7);
+
+        treeNode_1.left = treeNode_2;
+        treeNode_1.right = treeNode_3;
+        treeNode_3.left = treeNode_4;
+        treeNode_3.right = treeNode_5;
+
+        System.out.println(solution.postorderTraversal(treeNode_1));
+
+    }
+}
---
+++ Java/145. Binary Tree Postorder Traversal 1/src/com/dominic/Solution.java
@@ -0,0 +1,27 @@
+package com.dominic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    List<Integer> answer = new ArrayList<Integer>();
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if (root != null) {
+            postorderTraversal(root.left);
+            postorderTraversal(root.right);
+            answer.add(root.val);
+        }
+
+        return answer;
+    }
+}
